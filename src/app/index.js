f505bc99f9a1e8e5a4528fd83f2624b71f800d70
Alberto Mijares <almilo@almilo.es>
2016-03-08 15:35:30 +0100

    (refactor): remove redundant bootstrap import

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,6 @@
 import './index.css';
 import 'expose?jQuery!jquery';
 import 'angular';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import 'reflect-metadata';
 import { bootstrap } from 'ng-forward';
 import configModule from './config/index';
