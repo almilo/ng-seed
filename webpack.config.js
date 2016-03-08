d297db16cee77d904bf4eeb89b508fca4cf5311c
Alberto Mijares <alberto.mijares@edorasware.com>
2016-03-08 11:36:40 +0100

    (chore): add initial project configuration

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: app('index.js'),
+    output: {
+        path: 'dist',
+        filename: 'app.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'stage-0'],
+                    plugins: [
+                        'transform-decorators-legacy'
+                    ]
+                }
+            },
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('css')},
+            {test: /\.(woff|woff2|ttf|eot|svg)$/, loader: 'file?name=font/[name].[ext]'}
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css'),
+        new HtmlWebpackPlugin({
+            template: app('index.html'),
+            inject: 'body'
+        })
+    ]
+};
+
+function app(fileName) {
+    return path.join(__dirname, 'src', 'app', fileName);
+}
